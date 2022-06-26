a71d9a332e18fc84404b7e849bc421f0381bed69
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-06-26 16:39:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const firstParagraph = document.querySelector('#first')
 const border = document.querySelector('.border1')
 border.innerHTML = '<span>Hello I work</span>'
+
+
+firstParagraph.style.background = 'black';
